94cfb73f4360abc6777c0db1c51a553f87589cb6
Michal Wojciechowski <michal.wojciechowsk2@microsoft.wsei.edu.pl>
2023-10-18 19:05:55 +0200

    Labolatorium3

---
+++ Labolatorium3/Controllers/LibraryController.cs
@@ -0,0 +1,39 @@
+﻿using Labolatorium3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Labolatorium3.Controllers
+{
+    public class LibraryController : Controller
+    {
+        static readonly Dictionary<int, Book> _book= new Dictionary<int, Book>();
+        static int id = 1;
+        public IActionResult Index()
+        {
+            return View(_book);
+        }        
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book model) 
+        {
+            if (ModelState.IsValid)
+            {
+                model.Id = id++;
+                _book[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Update(int id )
+        {
+            return View(_book[id]);
+        }
+    }
+}
---
+++ Labolatorium3/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace Labolatorium3.Models
+{
+    public class Book
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter the book title")]
+        public string title { get; set; }
+        [StringLength(maximumLength: 100, ErrorMessage ="To much characters")]
+        public string author { get; set; }
+        [MaxLength(10000)]
+        public int numberOfPages { get; set; }
+        [MaxLength(13)]
+        public int ISBN { get; set; }
+        public DateTime publicationDate { get; set; }
+        public string publishingHouse { get; set; }
+
+    }
+}
